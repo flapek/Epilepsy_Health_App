e71e20de2302fb8e3783743f3a8446d795394b20
flapek <filap4@gmail.com>
2020-08-08 11:01:58 +0200

    cos

--- Scripts/git-commit-all.ps1
+++ Scripts/git-commit-all.ps1
@@ -16,31 +16,37 @@ try {
         Set-Location $reposLocation
         $lastRepo = $currentRepo
 
+        git checkout develop
         $currentBranch = git branch --show-current
         Write-Host "====================================================================================" -foreground blue
         Write-Host "Commit the Repository: "$repo -foreground Blue
         Write-Host "Current branch - "$currentBranch -foreground Yellow
         Write-Host "====================================================================================" -foreground blue
         Write-Host "Are you want commit changes on this branch?? (y/n)"
-        $choose Read-Host
-        if ($choose.ToLower() == "y") {
-            # git commit -m $commitComment
+        $choose = Read-Host
+        if ($choose.ToLower() -eq "y") {
+            git add .
+            git commit -m $commitComment
         }
         else {
-            Write-Host "Script change branch on develop and then create new branch." -foreground Blue
+            Write-Host "Script change branch on develop, then create new branch and push it to remote." -foreground Blue
             git branch
             Write-Host "Choose branch. (if name didn't exist, it will be created)" -foreground Blue
-            $newBranch Read-Host
-            git
+            $newBranch = Read-Host
+            git checkout develop
+            git checkout -b $newBranch
+            git branch -u origin $newBranch    
+            git add .
+            git commit -m $commitComment
         }
-        # Write-Host "====================================================================================" -foreground blue
-        
+        Write-Host "====================================================================================" -foreground blue
     }
     Set-Location $currentLocation
 
     Write-Host "Success!!" -foreground Green
 }
 catch {
+    Set-Location $currentLocation
     Write-Host "Error!!" -foreground Red    
     Write-Host "Scripts fail, check it!" -foreground Red
 }
