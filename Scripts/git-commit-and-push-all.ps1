d4669b0284e72abefcf5316e8033bff8210c0ec9
flapek <filap4@gmail.com>
2020-08-22 21:57:40 +0200

    updatescript

--- Scripts/git-commit-and-push-all.ps1
+++ Scripts/git-commit-and-push-all.ps1
@@ -9,6 +9,10 @@ try {
     $commitComment = Read-Host
     $reposLocation = $currentLocation -replace $toRemoveFromLocation, ""
     $lastRepo = "\\Epilepsy_Health_App"
+
+    Write-Host "Are you want commit changes on all branches?? (y/n)"
+    $choose = Read-Host
+
     foreach($repo in $repos) {
         $currentRepo = "\\$($repo)"
         
@@ -22,26 +26,30 @@ try {
         Write-Host "Commit the Repository: "$repo -foreground Blue
         Write-Host "Current branch - "$currentBranch -foreground Yellow
         Write-Host "====================================================================================" -foreground blue
-        Write-Host "Are you want commit changes on this branch?? (y/n)"
-        $choose = Read-Host
-        if ($choose.ToLower() -eq "n") {            
-            Write-Host "Script change branch on develop, then create new branch and push it to remote." -foreground Blue
-            $branches = git branch
-            Write-Host $branches
-            Write-Host "Choose branch. (if name didn't exist, it will be created)" -foreground Blue
-            $Branch = Read-Host
+       
+        if ($choose.ToLower() -eq "y") {
+            
+            Write-Host "Are you want commit changes on this branch?? (y/n)"
+            $choose = Read-Host
+            if ($choose.ToLower() -eq "n") {            
+                Write-Host "Script change branch on develop, then create new branch and push it to remote." -foreground Blue
+                $branches = git branch
+                Write-Host $branches
+                Write-Host "Choose branch. (if name didn't exist, it will be created)" -foreground Blue
+                $Branch = Read-Host
 
-            if ($Branch.Contains("master")) {
-                Write-Host "You cannot send changes on master branch, I swithc branch to develop!" -foreground Red
-                $Branch = "develop"
-            }
+                if ($Branch.Contains("master")) {
+                    Write-Host "You cannot send changes on master branch, I swithc branch to develop!" -foreground Red
+                    $Branch = "develop"
+                }
 
-            if ($branches.Contains($Branch)) {
-                git checkout $Branch
-            } else {
-                git checkout develop
-                git checkout -b $Branch
-                git push -u origin $Branch    
+                if ($branches.Contains($Branch)) {
+                    git checkout $Branch
+                } else {
+                    git checkout develop
+                    git checkout -b $Branch
+                    git push -u origin $Branch    
+                }
             }
         }
         git add .
